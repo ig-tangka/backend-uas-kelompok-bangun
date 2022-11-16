29efb22dad0cb1c7589dc3ac3c040eff39a441ee
ig-tangka <ignatiustangka@gmail.com>
2022-11-16 12:57:22 +0800

    minor change wisata.js & index.js commit

--- controller/wisata.js
+++ controller/wisata.js
@@ -1,4 +1,4 @@
-import Wisata from '../model/wisataModel'
+import Wisata from '../model/wisataModel.js'
 
 export const getWisata = async (req,res) => {
     try {
@@ -18,6 +18,42 @@ export const getWisataById = async (req, res) => {
             }
         })
         res.status(200).json(response);
+    } catch (error) {
+        console.log(error.messagge);
+    }
+}
+
+export const createWisata = async (req, res) => {
+    try {
+        // INSERT
+        await Product.create(req.body);
+        res.status(201).json({ msg: 'Product Created'})
+    } catch (error) {
+        console.log(error.messagge);
+    }
+}
+
+export const updateWisata = async (req, res) => {
+    try {
+        await Product.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json({ msg: 'Product Updated'});
+    } catch (error) {
+        console.log(error.messagge);
+    }
+}
+
+export const deleteWisata = async (req, res) => {
+    try {
+        await Product.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json({ msg: 'Product Deleted'})
     } catch (error) {
         console.log(error.messagge);
     }
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import wisataRoute from ''
+import wisataRoute from './routes/wisataRoute.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(wisataRoute);
+
+
+app.listen(5000, () => console.log('Server running on port 5000'));
