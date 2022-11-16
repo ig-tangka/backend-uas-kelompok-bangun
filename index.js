486c761b6998cf62b88a7548835d465f28079d35
ig-tangka <ignatiustangka@gmail.com>
2022-11-16 13:35:01 +0800

    test

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import wisataRoute from './routes/wisataRoute.js';
+import express from "express";
+import cors from "cors";
+import wisataRoute from "./routes/wisataRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(wisataRoute);
 
-
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5000, () => console.log("Server running on port 5000"));
--- model/wisataModel.js
+++ model/wisataModel.js
@@ -1,18 +1,22 @@
 import { Sequelize } from "sequelize";
-import db from '../config/Database.js'
+import db from "../config/Database.js"
 
 const { DataTypes } = Sequelize;
 
-const Wisata = db.define('wisata', {
+const Wisata = db.define(
+  "wisata",
+  {
     nama: DataTypes.STRING,
     deskripsi: DataTypes.VARCHAR,
     img: DataTypes.VARCHAR,
-}, {
-    freezeTableName: true
-});
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 export default Wisata;
 
-(async() => {
-    await db.sync();
-});
+(async () => {
+  await db.sync();
+})();
